0b54122fc4ddd81fcfe596b0715fb469321e0af1
Tushar3232 <anikdebnath916@gmail.com>
2025-03-02 00:18:57 +0600

    second commit

--- script/discover.js
+++ script/discover.js
@@ -1,3 +1,13 @@
+
+// show date in web site 
+const color=["#eccf74 ","#eca174","#bfe78a","#8ae7d5","#8ac8e7","#c18ae7 "];
+document.getElementById("bg_btn").addEventListener("click",function(event){
+    event.preventDefault();
+    let randomColor= color[Math.floor(Math.random()*color.length)];
+    document.body.style.background=randomColor;
+
+})
+
 document.getElementById("new_today").addEventListener("click",function(event){
     event.preventDefault();
     window.location.href="./discover.html";
--- script/main.js
+++ script/main.js
@@ -1,3 +1,41 @@
-document.getElementById("btn_1").addEventListener("click",function(){
+document.getElementById("btn_1").addEventListener("click",function(event){
+    event.preventDefault();
+    alert()
+    const num= 1 ;
+    // task uncomplite 
+    const task = document.getElementById("task").innerText ;
+    const updaTask = parseInt(task);
+    const incomplet = updaTask - num ;
+    document.getElementById("task").innerText = incomplet ; 
+
+    // total task complite 
+    const TotalTask = document.getElementById("Task_complet").innerText;
+    const updatTotalTask = parseInt(TotalTask);
+    const sum = updatTotalTask + num ;
+    document.getElementById("Task_complet").innerText = sum ;
     
+    this.disabled = true;
+
+})
+
+// The part of box 2 
+
+document.getElementById("btn_2").addEventListener("click",function(event){
+    event.preventDefault();
+    alert()
+    const num= 1 ;
+    // task uncomplite 
+    const task = document.getElementById("task").innerText ;
+    const updaTask = parseInt(task);
+    const incomplet = updaTask - num ;
+    document.getElementById("task").innerText = incomplet ; 
+
+    // total task complite 
+    const TotalTask = document.getElementById("Task_complet").innerText;
+    const updatTotalTask = parseInt(TotalTask);
+    const sum = updatTotalTask + num ;
+    document.getElementById("Task_complet").innerText = sum ;
+    
+    this.disabled = true;
+
 })
