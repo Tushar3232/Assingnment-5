3a1735ddeaaa097bc97aea65a32297fdd2177498
Tushar3232 <anikdebnath916@gmail.com>
2025-03-02 20:46:07 +0600

    third commit

--- script/discover.js
+++ script/discover.js
@@ -1,6 +1,6 @@
 
 // show date in web site 
-const color=["#eccf74 ","#eca174","#bfe78a","#8ae7d5","#8ac8e7","#c18ae7 "];
+const color=["#eccf74 ","#eca174","#bfe78a","#8ae7d5","#8ac8e7","#c18ae7 ","#efdae1","#81816d","#8795a5"];
 document.getElementById("bg_btn").addEventListener("click",function(event){
     event.preventDefault();
     let randomColor= color[Math.floor(Math.random()*color.length)];
@@ -19,3 +19,4 @@ document.getElementById("currentDate").innerText = new Date().toLocaleDateString
     month: 'long',
     day: 'numeric'
 });
+
